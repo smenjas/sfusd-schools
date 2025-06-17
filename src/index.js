76fc2bbaf93f3542a1f78238df21c609456ccded
Ben Thornton <smenjas@users.noreply.github.com>
2025-06-17 09:52:20 -0700

    Serve school data as JSON.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import fs from 'fs';
 import http from 'http';
 import url from 'url';
 
+import schoolData from '../public/school-data.js';
+
 const hostname = '127.0.0.1';
 const port = 3008;
 
@@ -21,6 +23,11 @@ const server = http.createServer((request, response) => {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/javascript');
         content = fs.readFileSync('public' + path, 'utf8');
+        break;
+    case '/schools.json':
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'application/json');
+        content = JSON.stringify(schoolData);
         break;
     case '/main.css':
         response.statusCode = 200;
