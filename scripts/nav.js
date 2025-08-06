a217c69891a0b47defd82be15b7836c806df0f31
Ben Thornton <smenjas@users.noreply.github.com>
2025-08-05 21:16:02 -0700

    Generate a KML file to display waypoints.

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,10 +2,96 @@
  * @file Navigate between places in San Francisco, California.
  */
 
-import { findSchoolDistances } from '../public/path.js';
+import { kmlDoc } from '../public/kml.js';
+import { findSchoolDistances,
+         findPathToSchool,
+         getAddressCoords,
+         getJunctionCoords,
+         getStreetJunctions,
+         nameCNN } from '../public/path.js';
+import { prettifyAddress } from '../public/address.js';
 import addressData from '../public/address-data.js';
 import schoolData from '../public/school-data.js';
 import jcts from '../public/sf-junctions.js';
+
+/**
+ * Find a school.
+ *
+ * @param {Schools} schoolData - All SF public schools
+ * @param {string} name - A school name, e.g. "Lowell"
+ * @param {string} type - A school type, e.g. "High"
+ * @returns {School} Data about a school
+ */
+function findSchool(schoolData, name, type) {
+    for (const school of schoolData) {
+        if (school.name === name && school.types.includes(type)) {
+            return school;
+        }
+    }
+}
+
+/**
+ * Generate waypoints.
+ *
+ * @param {StreetAddresses} addressData - All SF street addresses
+ * @param {Junctions} jcts - All SF intersections
+ * @param {string} start - The starting street address
+ * @param {string} end - The ending street address
+ * @returns {Array.<Object>} Waypoints
+ */
+function getPathWaypoints(addressData, jcts, path, start, end) {
+    const startLl = getAddressCoords(addressData, start);
+    const endLl = getAddressCoords(addressData, end);
+
+    let n = 1;
+    const startPretty = prettifyAddress(start);
+    const endPretty = prettifyAddress(end);
+    const wpts = [{ ll: startLl, name: n++, description: startPretty, sym: 'Start' }];
+    for (const cnn of path) {
+        wpts.push({
+            ll: getJunctionCoords(jcts, cnn),
+            name: n++,
+            description: nameCNN(jcts, cnn),
+            sym: 'Intersection',
+        });
+    }
+    wpts.push({ ll: endLl, name: n, description: endPretty, sym: 'End' });
+    return wpts;
+}
+
+/**
+ * Generate a file that describes a journey.
+ *
+ * @param {StreetAddresses} addressData - All SF street addresses
+ * @param {Junctions} jcts - All SF intersections
+ * @param {string} start - The starting street address
+ * @param {string} end - The ending street address
+ * @param {string} [place=''] - The name of the destination (optional)
+ * @returns {string} XML
+ */
+function makeGeoDoc(addressData, jcts, path, start, end, place = '') {
+    const startPretty = prettifyAddress(start);
+    const endPretty = (place !== '') ? place : prettifyAddress(end);
+    const name = `${startPretty} to ${endPretty}`;
+    const wpts = getPathWaypoints(addressData, jcts, path, start, end);
+    return kmlDoc(wpts, name);
+}
+
+/**
+ * Output a KML (Keyhole Markup Language) file.
+ *
+ * @param {StreetAddresses} addressData - All SF street addresses
+ * @param {Junctions} jcts - All SF intersections
+ * @param {string} start - The starting street address
+ * @param {School} school - Data about a school
+ */
+function logKML(addressData, jcts, start, school) {
+    const stJcts = getStreetJunctions(jcts);
+    const path = findPathToSchool(addressData, jcts, stJcts, {}, start, school);
+    const end = school.address;
+    const place = `${school.name} ${school.types[0]}`;
+    console.log(makeGeoDoc(addressData, jcts, path, start, end, place));
+}
 
 //const start = '443 Burnett Ave'; // Rooftop Elementary, geographic center of SF
 //const start = '999 Marine Dr'; // Fort Point
@@ -19,4 +105,13 @@ const start = '1700 Silver Ave'; // Silver Terrace Athletic Fields
 //const start = '2995 Sloat Blvd'; // SF Zoo
 //const start = '1096 Point Lobos Ave'; // Camera Obscura
 
+//const school = findSchool(schoolData, 'El Dorado', 'Elementary');
+//const school = findSchool(schoolData, 'Guadalupe', 'Elementary');
+//const school = findSchool(schoolData, 'Rooftop', 'Elementary');
+const school = findSchool(schoolData, 'Taylor', 'Elementary');
+//const school = findSchool(schoolData, 'Hoover', 'Middle');
+//const school = findSchool(schoolData, 'Burton', 'High');
+//const school = findSchool(schoolData, 'Life Learning', 'High');
+
+logKML(addressData, jcts, start, school); process.exit(0);
 const distances = findSchoolDistances(addressData, schoolData, jcts, start);
