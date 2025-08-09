2a7e62c877dabb0cd01f7d22a962c6c2d32d4c83
Ben Thornton <smenjas@users.noreply.github.com>
2025-08-08 20:10:51 -0700

    Compare commute distances.

--- public/path.js
+++ public/path.js
@@ -390,7 +390,7 @@ function findPath(addressData, jcts, stJcts, beelines, start, end, place = '') {
  * @param {?string} [end=null] - The ending street address
  * @returns {number} Distance in miles
  */
-export function sumDistances(addressData, jcts, path, start, end) {
+export function sumDistances(addressData, jcts, path, start = null, end = null) {
     if (!Array.isArray(path) || path.length < 1) {
         return howFarAddresses(addressData, start, end);
     }
