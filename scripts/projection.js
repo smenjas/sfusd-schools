40ebcd3efde1500664e5badd918c82d46c7b2a4d
Ben Thornton <smenjas@users.noreply.github.com>
2025-09-06 00:01:54 -0700

    Find the projection of an address to a street segment.

---
+++ scripts/projection.js
@@ -0,0 +1,18 @@
+/**
+ * @file Find the projection from an address to a street segment.
+ *
+ * @example
+ * % node projection.js 
+ * 100 John F Kennedy Dr [ 37.771520592913014, -122.46045492238001, 0.55192997231347 ]
+ */
+
+import { findAddressProjection } from '../public/geo.js';
+import addressData from '../public/address-data.js';
+import jcts from '../public/junctions.js';
+
+const cnnA = 27187;
+const cnnB = 27188;
+const address = '100 John F Kennedy Dr';
+
+const coords = findAddressProjection(addressData, jcts, cnnA, cnnB, address);
+console.log(address, coords);
